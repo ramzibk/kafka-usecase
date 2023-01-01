9331b04f7b35fbe879de34ed1f484be5630518d8
Ramzi Ben Khedhiri <bk.ramzi@gmail.com>
2023-01-01 21:52:23 +0100

    implement a kafka consumer

--- src/main/java/com/rbk/kafka/usecase/Main.java
+++ src/main/java/com/rbk/kafka/usecase/Main.java
@@ -1,10 +1,16 @@
 package com.rbk.kafka.usecase;
 
+
 public class Main {
+
+    public static final String TOPIC_NAME = "usecase.chat.messages";
 
     public static void main(String[] args) {
         SimpleKafkaProducer producer = new SimpleKafkaProducer("localhost:9092");
-        producer.publish("usecase.chat.messages", "new student registration "+System.currentTimeMillis());
-        producer.publish("usecase.chat.messages", "studentName", "John Doe");
+        producer.publish(TOPIC_NAME, "new student registration "+System.currentTimeMillis());
+        producer.publish(TOPIC_NAME, "studentName", "John Doe");
+
+        SimpleKafkaConsumer consumer = new SimpleKafkaConsumer("localhost:9092", TOPIC_NAME);
+        consumer.poll();
     }
 }
---
+++ src/main/java/com/rbk/kafka/usecase/SimpleKafkaConsumer.java
@@ -0,0 +1,50 @@
+package com.rbk.kafka.usecase;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class SimpleKafkaConsumer {
+
+    private final KafkaConsumer<String, String> consumer;
+
+    private String topic;
+
+    public SimpleKafkaConsumer(String brokerURI, String topic) {
+        super();
+        this.topic = topic;
+
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerURI);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "ClientAppGroup");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        consumer = new KafkaConsumer<String, String>(config);
+        consumer.subscribe(Collections.singletonList(this.topic));
+    }
+
+    public void poll() {
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(60));
+
+            for (ConsumerRecord rec : records) {
+                System.out.println("Message received with key: " + rec.key() + " and value: " + rec.value());
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+}
